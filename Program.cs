9ae174d1bec42a60999508434b71740f7de825ea
filipwierzba <filip.wierzbaa@gmail.com>
2022-02-05 19:28:43 +0100

    pseudo RNG

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Calculator
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Welcome to Random Number Generator");
+            Console.WriteLine("Select your range");
+            Console.WriteLine("Enter range minimum:");
+            int mini = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter range maximum:");
+            int maxi = Convert.ToInt32(Console.ReadLine());
+            Random rand = new Random();
+            int rng = rand.Next(mini, maxi);
+            Console.WriteLine($"Your random number is: {rng}");
+            
+
+
+
+        }
+    }
+}
